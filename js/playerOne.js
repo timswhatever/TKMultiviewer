b97265b96dcc5c3a0bd7d1fd991831f33d7470e8
tim <tim48433@gmail.com>
2024-11-22 20:14:17 -0500

    Fixed mistake

--- js/playerOne.js
+++ js/playerOne.js
@@ -2,7 +2,7 @@ document.getElementById('url1').addEventListener("url1", submitOne);
 
 function submitOne() {
     let channelOne = document.getElementById('url1').value
-    var combined = "https://player.twitch.tv/?channel=" + channelOne + "?&parent=timswhatever.github.io"
+    var combined = "https://player.twitch.tv/?channel=" + channelOne + "&parent=timswhatever.github.io"
     document.getElementById('channel1').setAttribute("src", combined)
 }
 
