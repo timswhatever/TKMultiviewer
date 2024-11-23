8c66778c3b78246c4023f48daf972729372cfc8a
tim <tim48433@gmail.com>
2024-11-22 20:05:43 -0500

    experiment

--- js/playerOne.js
+++ js/playerOne.js
@@ -2,19 +2,9 @@ let channelOne = "Twitch"
 
 document.getElementById('url1').addEventListener("url1", submitOne);
 
-new Twitch.Embed("twitch-embed", {
-    width: 854,
-    height: 480,
-    channel: [channelOne],
-    autoplay: true,
-    layout: "video",
-    muted: false,
-    theme: "dark",
-    });
-
 function submitOne() {
     channelOne = document.getElementById('url1').value
-    console.log(channelOne)
+    document.getElementById('Channel1').src = 'https://player.twitch.tv/?channel='+channelOne+'&parent=timswhatever.github.io'
 }
 
 
