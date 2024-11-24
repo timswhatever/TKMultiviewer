73550e79d6d64351a58f3a631bc1971d496da178
tim <tim48433@gmail.com>
2024-11-24 14:40:48 -0500

    Added kick support

--- js/playerTwo.js
+++ js/playerTwo.js
@@ -3,5 +3,12 @@ document.getElementById('url2').addEventListener("url2", submitTwo);
 function submitTwo() {
     let channelTwo = document.getElementById('url2').value
     var combined = "https://player.twitch.tv/?channel=" + channelTwo + "&parent=timswhatever.github.io"
-    document.getElementById('channel2').setAttribute("src", combined)
+    if (checkbox2.checked == true) {
+        var combined2 = "https://player.kick.com/" + channelTwo
+        document.getElementById('channel2').setAttribute("src", combined2)
+    } else {
+        var combined = "https://player.twitch.tv/?channel=" + channelTwo + "&parent=timswhatever.github.io"
+        document.getElementById('channel2').setAttribute("src", combined)
+    }
+    channelTwo = "";
 }
