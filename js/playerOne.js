0ae7a075701ecaeadbf0aaeabdc8944a6d50116c
tim <tim48433@gmail.com>
2024-11-24 14:16:53 -0500

    Testing kick + twitch

--- js/playerOne.js
+++ js/playerOne.js
@@ -2,8 +2,13 @@ document.getElementById('url1').addEventListener("url1", submitOne);
 
 function submitOne() {
     let channelOne = document.getElementById('url1').value
-    var combined = "https://player.twitch.tv/?channel=" + channelOne + "&parent=timswhatever.github.io"
-    document.getElementById('channel1').setAttribute("src", combined)
+    if (document.getElementById('checkbox1').checked) {
+        var combined2 = "https://player.kick.com/" + channelOne
+        document.getElementById('channel1').setAttribute("src", combined2)
+    } else {
+        var combined = "https://player.twitch.tv/?channel=" + channelOne + "&parent=timswhatever.github.io"
+        document.getElementById('channel1').setAttribute("src", combined)
+    }
 }
 
 
