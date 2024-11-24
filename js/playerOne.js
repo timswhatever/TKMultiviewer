89acbdba0d7241314049880bf6d5f88c2fc49d4a
tim <tim48433@gmail.com>
2024-11-24 14:22:03 -0500

    Fix

--- js/playerOne.js
+++ js/playerOne.js
@@ -2,7 +2,8 @@ document.getElementById('url1').addEventListener("url1", submitOne);
 
 function submitOne() {
     let channelOne = document.getElementById('url1').value
-    if (document.getElementById('checkbox1').checked) {
+    var checkbox1 = document.getElementById('checkbox1');
+    if (checkbox1.ariaChecked == true) {
         var combined2 = "https://player.kick.com/" + channelOne
         document.getElementById('channel1').setAttribute("src", combined2)
     } else {
